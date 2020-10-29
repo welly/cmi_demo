27f2c438a1dfa935b59526f11725c25ee0b30762
Alastair Moore <alastair.moore@manifesto.co.uk>
2020-10-29 20:23:06 +0000

    Development split config created, added devel to split

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,3 +4,5 @@
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+
+$config['config_split.config_split.config_dev']['status'] = TRUE;
